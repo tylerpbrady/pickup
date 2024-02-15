452e4a71267dff92da4c5114d7dd083c477e31e8
Tyler Brady <tylerpbrady@gmail.com>
2024-02-15 13:03:21 -0800

    Added basic functions in game-services.js

--- packages/express-backend/models/user.js
+++ packages/express-backend/models/user.js
@@ -2,23 +2,38 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    name: {
+    userName: {
       type: String,
       required: true,
       trim: true,
     },
-    job: {
+    email: {
       type: String,
       required: true,
       trim: true,
-      validate(value) {
-        if (value.length < 2)
-          throw new Error("Invalid job, must be at least 2 characters.");
-      },
     },
+
+    // password will definitely need to be changed
+    // just putting it here now as placeholder
+
+    password: {
+      type: String,
+      required: true,
+      trim: false,
+    },
+    games: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Game",
+    }]
   },
   { collection: "users_list" }
 );
+
+// validate(value) {
+//   if (value.length < 2)
+//     throw new Error("Invalid job, must be at least 2 characters.");
+// }
+
 
 const User = mongoose.model("User", UserSchema);
 
