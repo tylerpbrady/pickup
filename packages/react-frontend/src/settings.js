13cef47ca028b3e0c3539b4886a78634eb3218e2
Andrew Okerlund <apokerlu@calpoly.edu>
2024-02-28 14:33:35 -0800

    settings buttons

---
+++ packages/react-frontend/src/settings.js
@@ -0,0 +1,18 @@
+// settings.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './settings.css';
+
+
+function Settings() {
+    return (
+        //<div className="settings">
+        //<div className='rounded-rectangle' style={rectangleStyles}>
+            <div>
+                <Link to="/log out" className="log-out-button">Logout</Link>
+                <Link to="/edit profile" className="edit-profile-button">Profile</Link> 
+            </div>            
+      );
+}
+
+export default Settings;
