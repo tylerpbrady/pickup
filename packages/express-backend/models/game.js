2efb3244ff62c906586491af360e6d85ded2a228
Damon <damonlin916@gmail.com>
2024-03-04 19:22:22 -0800

    fixing merge conflict errors?

--- packages/express-backend/models/game.js
+++ packages/express-backend/models/game.js
@@ -1,62 +1,11 @@
 import mongoose from "mongoose";
 
 const GameSchema = new mongoose.Schema(
-<<<<<<< HEAD
   {
     title: {
       type: String,
       required: true,
       trim: true,
-=======
-    {
-        title: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        // commenting out for simplicity
-        // hostId: {
-        //     type: mongoose.Schema.Types.ObjectId,
-        //     required: true,
-        //     ref: "User",
-        // },
-        sport: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        description: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        location: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        maxPlayers: {
-            type: Number,
-            required: false,
-        },
-        // players: [{
-        //     type: mongoose.Schema.Types.ObjectId,
-        //     ref: "User",
-        // }],
-        time: {
-            type: Date,
-            required: true,
-        },
-        equipment: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        skill: {
-            type: Number,
-            required: true,
-        }
->>>>>>> main
     },
     // commenting out for simplicity
     // hostId: {
--- packages/react-frontend/src/Header.js
+++ packages/react-frontend/src/Header.js
@@ -4,7 +4,6 @@ import "./Header.css";
 
 const Header = () => {
   return (
-<<<<<<< HEAD
     <div
       className="header"
       style={{
@@ -13,15 +12,11 @@ const Header = () => {
         alignItems: "center",
       }}
     >
-=======
-    <div className="header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingBottom: '40px' }}>
->>>>>>> main
       <h1 className="large-title">
         <Link to="/home" style={{ textDecoration: "none", color: "inherit" }}>
           Pickup
         </Link>
       </h1>
-<<<<<<< HEAD
       <div className="header-right">
         <Link to="/create-game" className="create-game-button">
           Create Game
@@ -32,12 +27,6 @@ const Header = () => {
         <Link to="/settings" className="settings-button">
           Settings
         </Link>
-=======
-      <div>
-        <Link to="/create-game" className="create-game-button">Create Game</Link>
-        <Link to="/profile" className="profile-button">Profile</Link>
-        <Link to="/settings" className="settings-button">Settings</Link>
->>>>>>> main
       </div>
     </div>
   );
