8cd91752d4a1ed17c3b3f6ba3917ae10dc4e2e11
Damon <damonlin916@gmail.com>
2024-03-06 10:20:58 -0800

    getting rid of some debug statements

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send(`This is the env var: ${process.env.TOKEN_SECRET}`);
+  res.send("hi");
 });
 
 app.get("/users", auth.authenticateUser, (req, res) => {
