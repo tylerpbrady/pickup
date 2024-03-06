d3de95c208acaaf54820b7bf9188b023e3c99871
Damon <damonlin916@gmail.com>
2024-03-05 20:51:37 -0800

    restarting deployment since it failed for some reason

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.send(process.env.TOKEN_SECRET);
 });
 
 app.get("/users", auth.authenticateUser, (req, res) => {
