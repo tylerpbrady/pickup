0f22cab9167232c4b59356c4b3a8e76f6da7b798
Andrew Okerlund <apokerlu@calpoly.edu>
2024-03-10 21:23:19 -0700

    profile

--- packages/react-frontend/src/settings.js
+++ packages/react-frontend/src/settings.js
@@ -1,18 +1,31 @@
 // settings.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './settings.css';
 
 
+
 function Settings() {
+    // function Prof(){
+    //     return (
+    //         <div>
+    //           <Profile_form Profile_form/>
+    //         </div>
+    //       );
+    // }
     return (
         //<div className="settings">
         //<div className='rounded-rectangle' style={rectangleStyles}>
-            <div>
-                <Link to="/log out" className="log-out-button">Logout</Link>
-                <Link to="/edit profile" className="edit-profile-button">Profile</Link> 
-            </div>            
+        <div>
+          <Link to="/welcome" className="log-out-button">
+            Logout
+          </Link>
+          <Link to="/edit-profile" className="edit-profile-button">
+            Profile
+          </Link>
+        </div>
       );
-}
-
+    }
 export default Settings;
+
+//<Edit_profile path = "/edit-profile" updateProfileList = {updateProfileList}/>
