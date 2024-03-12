7f41796ce0ab6d7cd77f292dff916d63a54ca805
Andrew Okerlund <apokerlu@calpoly.edu>
2024-03-12 11:02:28 -0700

    Profile

--- packages/react-frontend/src/profile.js
+++ packages/react-frontend/src/profile.js
@@ -3,70 +3,12 @@ import React, { useState } from "react";
 
 function Profile_form(props) {
   const [profile, setProfile] = useState({
-
-
-
-
     name: "",
     sports_of_interest: "",
     city: ""
   });
 
 
-
-
- 
-  function postProfile(profile) {
-    console.log(profile)
-    const promise = fetch("http://localhost:8000/profile", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(profile),
-    });
-
-
-    return promise;
-  }
-
-
-  function updateProfileList(profile) {
-    postProfile(profile)
-      .then(response => {
-        if (response.status === 201) {
-          return response.json();
-        } else {
-          console.log('Failed to update list. Invalid HTTP Code (not 201).');
-        }
-      })
-      .then(updatedProfile => {
-        console.log(updatedProfile.profile)
-        setProfile([...profile, updatedProfile]);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  }
-
-
-//   function Edit_profile() {
-//     return(
-//       <div>
-       
-//         <Profile_form edit_profile = {updateProfileList}/>
-//       </div>
-//     )
-//   }
-
-
-  //const [selectedDate, setSelectedDate] = useState('');
-  //const [selectedTime, setSelectedTime] = useState('');
- 
-
-
-  // handle change and submit form has hacky fixes inplace
-  // Will change when expanded
   function EditProfile(event) {
     const { name, value } = event.target;
     if (name === "sports of interest")
