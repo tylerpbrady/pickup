e1a2d5c2d7bdc9170aa6dfee7152e0187d4361ba
Tyler Brady <tylerpbrady@gmail.com>
2024-03-14 14:19:37 -0700

    Added documentation for game-services.js

--- packages/express-backend/models/game-services.js
+++ packages/express-backend/models/game-services.js
@@ -3,26 +3,32 @@ import gameModel from "./game.js";
 
 mongoose.set("debug", true);
 
+// finds a game by its id
 function findGameById(id) {
   return gameModel.findById(id);
 }
 
+// finds a game by its title
 function findGameByTitle(title) {
   return gameModel.find({ title: title });
 }
 
+// gets game list
 function getGames() {
   return gameModel.find();
 }
 
+// gets array of players signed up for a game
 function getPlayersInGame(gameId) {
   return findGameById(gameId).populate("players");
 }
 
+// creates new game
 function createGame(game) {
   return new gameModel(game).save();
 }
 
+// deletes game
 function deleteGame(id) {
   return gameModel.findByIdAndDelete(id);
 }
