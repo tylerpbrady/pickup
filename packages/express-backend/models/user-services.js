c9b7647514ea484909742e2a9a9aa015ef24159f
Tyler Brady <tylerpbrady@gmail.com>
2024-03-14 14:20:52 -0700

    Added documentation to user-services.js

--- packages/express-backend/models/user-services.js
+++ packages/express-backend/models/user-services.js
@@ -3,16 +3,19 @@ import userModel from "./user.js";
 
 mongoose.set("debug", true);
 
+// creates a user
 function addUser(user) {
   const userToAdd = new userModel(user);
   const promise = userToAdd.save();
   return promise;
 }
 
+// retrieves a user by name
 function getUser(name) {
   return userModel.find({ username: name });
 }
 
+// updates user profile
 function updateUser(name, profile) {
   // parse profile into JSON.
   return userModel.updateOne({name: name}, profile)
